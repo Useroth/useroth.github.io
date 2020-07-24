5ccb6ae0f310341080315a79d3f55f4304e0a0f8
Useroth <useroth@cock.li>
2020-07-24 19:15:07 +0200

    This looks a bit better, finally.

--- spess-parallax/js/parallax.js
+++ spess-parallax/js/parallax.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   var container = document.querySelector("[data-parallax]");
   var childNodes = container.children;
   for (var n = 0; n < childNodes.length; n++) {
-    childNodes[n].setAttribute("data-index", -(n + 1));
+    childNodes[n].setAttribute("data-index", n + 1);
   }
   container.addEventListener("mousemove", function (e) {
     var elms = this.children;
@@ -10,12 +10,10 @@ window.addEventListener("load", function () {
     var windowCenterY = window.innerHeight / 2;
     for (var c = 0; c < elms.length; c++) {
       var motion = parseInt(elms[c].getAttribute("data-index")) / 40;
-      var moveX =
-        ((windowCenterX - e.pageX) / window.innerWidth) * 100 * motion + 50;
-      var moveY =
-        ((windowCenterY - e.pageY) / window.innerHeight) * 100 * motion + 50;
-      var x = moveX + "%";
-      var y = moveY + "%";
+      var moveX = (windowCenterX - e.pageX) * motion + 50;
+      var moveY = (windowCenterY - e.pageY) * motion + 50;
+      var x = (windowCenterX + moveX) + "px";
+      var y = (windowCenterY + moveY) + "px";
       elms[c].style.backgroundPosition = x + " " + y;
     }
   });
