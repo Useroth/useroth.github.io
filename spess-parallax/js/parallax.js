992acdc489cb6670fd679ee336e3c87b1458ec95
Useroth <useroth@cock.li>
2020-07-24 16:16:12 +0200

    test

---
+++ spess-parallax/js/parallax.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+  var container = document.querySelector("[data-parallax]");
+  var childNodes = container.children;
+  for (var n = 0; n < childNodes.length; n++) {
+    childNodes[n].setAttribute("data-index", n + 1);
+  }
+  container.addEventListener("mousemove", function (e) {
+    var elms = this.children;
+    var windowCenterX = window.innerWidth / 2;
+    var windowCenterY = window.innerHeight / 2;
+    for (var c = 0; c < elms.length; c++) {
+      var motion = parseInt(elms[c].getAttribute("data-index")) / 40;
+      var moveX =
+        ((windowCenterX - e.pageX) / window.innerWidth) * 100 * motion + 50;
+      var moveY =
+        ((windowCenterY - e.pageY) / window.innerHeight) * 100 * motion + 50;
+      var x = -moveX + "%";
+      var y = -moveY + "%";
+      elms[c].style.backgroundPosition = x + " " + y;
+      console.log(x, y);
+    }
+  });
+});
