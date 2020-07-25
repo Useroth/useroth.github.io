b2e061e936dcc39e538edfaa87814b3201ae49f2
Useroth <useroth@cock.li>
2020-07-25 03:03:40 +0200

    Gyro test.

--- spess-parallax/js/parallax-bg.js
+++ spess-parallax/js/parallax-bg.js
@@ -1,26 +1,50 @@
+var parallaxContainer = document.querySelector("[data-parallax]");
+var parallaxElements = parallaxContainer.children;
+
 window.addEventListener("load", function () {
-  var container = document.querySelector("[data-parallax]");
-  var childNodes = container.children;
-  for (var n = 0; n < childNodes.length; n++) {
-    var compStyle = getComputedStyle(childNodes[n]);
-    childNodes[n].setAttribute("data-initial-x", compStyle.backgroundPositionX);
-    childNodes[n].setAttribute("data-initial-y", compStyle.backgroundPositionY);
+  initParallax();
+});
+
+function initParallax() {
+  for (var n = 0; n < parallaxElements.length; n++) {
+    var compStyle = getComputedStyle(parallaxElements[n]);
+    parallaxElements[n].setAttribute(
+      "data-initial-x",
+      compStyle.backgroundPositionX
+    );
+    parallaxElements[n].setAttribute(
+      "data-initial-y",
+      compStyle.backgroundPositionY
+    );
   }
 
-  container.addEventListener("mousemove", function (e) {
-    var elms = this.children;
-    var windowCenterX = window.innerWidth / 2;
-    var windowCenterY = window.innerHeight / 2;
-    for (var c = 0; c < elms.length; c++) {
-      var motion = 1/parseInt(elms[c].getAttribute("data-depth"));
-      var initialX = elms[c].getAttribute("data-initial-x");
-      var initialY = elms[c].getAttribute("data-initial-y");
-      var moveX = (windowCenterX - e.pageX) * motion;
-      var moveY = (windowCenterY - e.pageY) * motion;
-      var x = moveX + "px";
-      var y = moveY + "px";
-      elms[c].style.backgroundPosition = `calc(${initialX} + ${x}) calc(${initialY} + ${y})`;
-      console.log(initialX,x,initialY,y)
-    }
-  });
-});
+  if (window.Gyroscope) {
+    var gyroscope = new Gyroscope({ frequency: 60 });
+    gyroscope.addEventListener("reading", (e) => {
+      calcParallax(gyroscope.x, gyroscope.y);
+    });
+    gyroscope.start();
+  } else {
+    parallaxContainer.addEventListener("mousemove", function (e) {
+      calcParallax(e.pageX, e.pageY);
+    });
+  }
+}
+
+function calcParallax(inputX, inputY) {
+  var windowCenterX = window.innerWidth / 2;
+  var windowCenterY = window.innerHeight / 2;
+  for (var c = 0; c < parallaxElements.length; c++) {
+    var motion = 1 / parseInt(parallaxElements[c].getAttribute("data-depth"));
+    var initialX = parallaxElements[c].getAttribute("data-initial-x");
+    var initialY = parallaxElements[c].getAttribute("data-initial-y");
+    var moveX = (windowCenterX - inputX) * motion;
+    var moveY = (windowCenterY - inputY) * motion;
+    var x = moveX + "px";
+    var y = moveY + "px";
+    parallaxElements[
+      c
+    ].style.backgroundPosition = `calc(${initialX} + ${x}) calc(${initialY} + ${y})`;
+    console.log(initialX, x, initialY, y);
+  }
+}
